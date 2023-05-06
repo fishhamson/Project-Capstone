9d57fa6c2d734a70a3a97626824bab3359fbb714
HoangPhuc <hoangphuc311tn@gmail.com>
2023-05-06 12:38:37 +0700

    update Header

---
+++ JS/header.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+    var active = document.getElementById("active");
+    active.classList.toggle("colorFix", window.scrollY > 0);
+    var colorText = document.getElementById("colorText1");
+    colorText.classList.toggle("colorText", window.scrollY > 0)
+    var colorText = document.getElementById("colorText2");
+    colorText.classList.toggle("colorText", window.scrollY > 0)
+    var colorText = document.getElementById("colorText3");
+    colorText.classList.toggle("colorText", window.scrollY > 0)
+    var colorText = document.getElementById("colorText4");
+    colorText.classList.toggle("colorText", window.scrollY > 0)
+    var colorLogo = document.getElementById("colorLogo1");
+    colorLogo.classList.toggle("colorLogo", window.scrollY > 0)
+    var colorLogo = document.getElementById("colorLogo2");
+    colorLogo.classList.toggle("colorLogo", window.scrollY > 0)
+  })
